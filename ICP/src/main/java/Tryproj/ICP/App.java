9a81b68a728df6350967d86e5f18e169a7e5f552
monica <monica.chre@gmail.com>
2018-12-09 22:57:20 -0600

    commiting

---
+++ ICP/src/main/java/Tryproj/ICP/App.java
@@ -0,0 +1,13 @@
+package Tryproj.ICP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
